b431bd2eb785e2bf072b4d9dc9c1c07d66903442
darkbright <foxtori8946@gmail.com>
2019-06-05 10:37:13 +0900

    start stop 토글 버튼 구현

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,34 @@
 import React, {Component} from "react";
 
 class Stopwatch extends Component {
+  state = {
+    isRunning: false
+  }
+  
+  handleStopwatch = () => {
+    this.setState(prevState => ({isRunning: !prevState.isRunning}));
+  }
+
+  tickRef;
+  
+  // DOM이 렌더링 된 직후에 호출되는 라이프 사이클
+  // 3rd 라이브러리 로딩, 네트워크 호출
+  componentDidMount() {
+    this.tickRef = setInterval(this.tick, 1000);
+  }
+  
+  // DOM이 파괴되기 직전에 호출되는 라이프 사이클
+  // 리소스 해제 등등
+  omponentWillUnmount() {
+    clearInterval(this.tickRef);
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? "Stop" : "Start"}</button>
         <button>Reset</button>
       </div>
     )
