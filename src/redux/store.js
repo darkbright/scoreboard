51054736cf513d3cdee2a2afaabab7dbec3f65f3
darkbright <foxtori8946@gmail.com>
2019-06-07 10:36:35 +0900

    title state dispatch
    devtools에서 직접 액션을 dispatch하는 테스트
    
    {
    type: 'player/UPDATE_TITLE', title: 'test scoreboard'
    }

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,21 @@
 import {combineReducers, createStore} from "redux";
 
+import {UPDATE_TITLE} from "./actionTypes";
+
 const playerInitialState = {
   title: 'My Scoreboard',
 }
 
 const playerReducer = (state = playerInitialState, action) => {
-  return state;
+  switch(action.type) {
+    case UPDATE_TITLE:
+      return {
+        ...state,
+        title: action.title
+      }
+    default: 
+      return state;
+  }
 }
 
 const rootReducer = combineReducers({playerReducer});
